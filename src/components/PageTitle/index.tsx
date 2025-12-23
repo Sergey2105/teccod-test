3bee91d4cb9a52b91e737075c6e65c58a65fca42
Sergey Roschin <Sergey210500@gmail.com>
2025-12-23 12:38:41 +0300

    fix

---
+++ src/components/PageTitle/index.tsx
@@ -0,0 +1,17 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router";
+
+interface PageTitleProps {
+    title: string;
+}
+const PageTitle = ({ title }: PageTitleProps) => {
+    const location = useLocation();
+
+    useEffect(() => {
+        document.title = title;
+    }, [location, title]);
+
+    return null;
+};
+
+export default PageTitle;
--- src/components/pages/Home/index.tsx
+++ src/components/pages/Home/index.tsx
@@ -1,10 +1,14 @@
 import ListService from "@/components/ListService";
 import styles from "./index.module.scss";
+import PageTitle from "@/components/PageTitle";
 
 export default function Home() {
     return (
-        <div className={styles["wrapper"]}>
-            <ListService className={styles["list"]} />
-        </div>
+        <>
+            <PageTitle title="Главная страница" />
+            <div className={styles["wrapper"]}>
+                <ListService className={styles["list"]} />
+            </div>
+        </>
     );
 }
